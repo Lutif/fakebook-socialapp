cb2219551b6cbbb9bfb146153661f0dbe1dfdab6
lutif <lmandhro@gmail.com>
2020-02-29 15:26:46 +0500

    add post router

---
+++ routes/api/posts/posts.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const fs = require('fs');
+const path = require('path');
+const fileName = path.basename(__filename);
+
+fs.readdir(__dirname, (err, files) => {
+  if (err) console.error(err);
+  let subRouts = [];
+  files.forEach(file => {
+    if (file != fileName) {
+      subRouts.push(require(`./${file}`));
+    }
+  });
+  //   console.log(subRouts);
+  router.use(subRouts);
+});
+
+module.exports = router;
