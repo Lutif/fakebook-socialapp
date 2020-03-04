c40071f80a86dc3b1deed828145c40298f0c68d7
lutif <lmandhro@gmail.com>
2020-03-04 15:38:41 +0500

    add data model for post state

---
+++ models/Posts.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  avatar: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
+});
+
+const Posts = new mongoose.model('post', postSchema);
+
+module.exports = Posts;
+//text
+//user
+//time
+//likes
+////user
+//comment
+////user
+////time
+////text
