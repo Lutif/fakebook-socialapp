6090bb4a3d477d2b5d8e659d9c76423d789877f0
lutif <lmandhro@gmail.com>
2020-03-05 15:39:09 +0500

    add data model for profile state

---
+++ models/Profile.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  github: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: String,
+        required: true
+      },
+      to: {
+        type: String
+      },
+      current: {
+        type: Boolean,
+        default: false
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: String,
+        required: true
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      to: {
+        type: String
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    facebook: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    youtube: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = new mongoose.model('profile', profileSchema);
