230def248aaf8b1674a6805ebecd7d6862a6f223
lutif <lmandhro@gmail.com>
2020-03-21 21:59:41 +0500

    deployment ready

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json({ useUrlExtended: false }));
 connectDB();
 app.use("/api", router);
 
-if (process.env.NODE_ENV == "Production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
